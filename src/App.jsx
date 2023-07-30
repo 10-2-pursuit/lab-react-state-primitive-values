c26b51a4d0e156e53de177f7878caada5116eeed
Elisaul Batista <elisaulbatista@Elisauls-MacBook-Air.local>
2023-07-30 17:17:47 -0400

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
-function App () {
-    return (
-      <main>
-        <p>React State Lab</p>
-      </main>
-    );
+function App() {
+  const [score, setScore] = useState(0);
+  const [incrementValue, setIncrementValue] = useState(1);
+  const [victory, setVictory] = useState(false);
+
+  const handleIncrement = () => {
+    setScore(score + incrementValue);
+
+    if (score + incrementValue >= 100) setVictory(true);
+  };
+
+  const handlePayButtonClick = () => {
+    if (score >= 10) {
+      setScore(score - 10);
+      setIncrementValue(incrementValue + 1);
+    } else {
+      alert("You can't afford that!");
+    }
+  };
+  const handlePlayAgainClick = () => {
+    setScore(0);
+    setIncrementValue(1);
+    setVictory(false);
+  };
+
+
+  return (
+    <div className="App">
+      <h1>Clicker Game</h1>
+      <p>Current Score: {score}</p>
+      {!victory && (
+        <>
+          <button onClick={handleIncrement}>+{incrementValue}</button>
+          <button onClick={handlePayButtonClick}>
+            Pay 10 points to change from +{incrementValue} to +{incrementValue + 1}
+          </button>
+        </>
+      )}
+      {victory && (
+        <>
+          <h2>You Win!</h2>
+          <button onClick={handlePlayAgainClick}>Play again?</button>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default App;
