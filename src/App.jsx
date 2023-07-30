038ff468298cf6296ac9da615b4204eac4356723
Tonesha Rose <131703989+tonesharose31@users.noreply.github.com>
2023-07-30 16:37:07 -0400

    created a function for score and increment

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import React from "react";
+import { useState } from "react";
 import "./App.css";
 
 function App () {
+  function addToScore(){
+    setScore(score + increment); 
+  }
+
+  function upgradeIncrement(){
+    if(score>= 10){
+      setIncrement(increment + 1); 
+      setScore(score - 10);
+    }
+    else{ 
+      alert("You can't afford that!");
+    }
+  }
+function reset() {
+  setScore(0);
+}
     return (
       <main>
-        <p>React State Lab</p>
+        <p></p>
       </main>
     );
 }
