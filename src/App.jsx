5e342e0ac21ab8b1e0309b8d5433289f4082eaeb
salvagebar <salvagebar@gmail.com>
2023-07-30 17:54:17 -0400

    Incrementor working

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,17 @@ function App() {
     setCounter((counter += increment));
   };
 
+  const addIncrement = () => {
+    setCounter(counter - 10);
+    setIncrement(increment + 1);
+  };
+
   return (
     <main>
       <h2>Current Score: {counter}</h2>
       <button onClick={addScore}>+{increment}</button>
       <br />
-      <button>
+      <button onClick={addIncrement}>
         Pay 10 Points To Change From +{increment} To +{increment + 1}
       </button>
     </main>
