83b9c3152d1a1cd9d99c2e4eb5d9a28900d84826
salvagebar <salvagebar@gmail.com>
2023-07-30 17:50:51 -0400

    Adding counter works

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,24 @@ import React from "react";
 import { useState } from "react";
 import "./App.css";
 
-function App () {
-    return (
-      <main>
-        <h2>Current Score: </h2>
-      </main>
-    );
+function App() {
+  let [counter, setCounter] = useState(0);
+  let [increment, setIncrement] = useState(1);
+
+  const addScore = () => {
+    setCounter((counter += increment));
+  };
+
+  return (
+    <main>
+      <h2>Current Score: {counter}</h2>
+      <button onClick={addScore}>+{increment}</button>
+      <br />
+      <button>
+        Pay 10 Points To Change From +{increment} To +{increment + 1}
+      </button>
+    </main>
+  );
 }
 
 export default App;
