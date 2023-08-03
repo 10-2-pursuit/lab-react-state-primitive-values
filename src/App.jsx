6ae8cd3a1e8291c13499979921b5581642484d55
Nicole Slater <nicoleslater@pursuit.org>
2023-08-03 16:37:02 -0400

    Css

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,54 @@
-
-import React from "react";
-import React, { useState } from 'react';
-import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
-  let [counter, setCounter] = useState(0);
-  let [increment, setIncrement] = useState(1);
+  const [counter, scoreCounter] = useState(0);
+  const [incrementor, setIncrementor] = useState(1);
+  const [gameOver, setGameOver] = useState(false);
 
-  const addScore = () => {
-    setCounter((counter += increment));
+  useEffect(() => {
+    if (counter >= 100) {
+      setGameOver(true);
+    }
+  }, [counter]);
+
+  const scoreTrack = () => {
+    scoreCounter(counter + incrementor);
   };
 
-  const addIncrement = () => {
-    counter > 9
-      ? (setCounter(counter - 10), setIncrement(increment + 1))
-      : alert("You can't afford that!");
+  const upgrade = () => {
+    if (counter >= 10) {
+      setIncrementor(incrementor + 1);
+      scoreCounter(counter - 10);
+    } else {
+      return alert("You can't afford that!");
+    }
   };
 
   const resetGame = () => {
-    setCounter((counter = 0));
-    setIncrement((increment = 1));
+    scoreCounter(0);
+    setIncrementor(1);
+    setGameOver(false)
   };
 
   return (
     <main>
-      <h2>Current Score: {counter}</h2>
-      {counter < 99 ? (
-        <>
-        <button onClick={addScore}>+{increment}</button>
-          <p></p>
-          <button onClick={addIncrement}>
-            Pay 10 points to change from +{increment} to +{increment + 1}
-          </button>
-        </>
-      ) : (
-        <>
-          <h2>You have won!</h2>
+      {gameOver ? (
+        <div>
+          <h2 className="game-score">Current Score: {counter} </h2>
+          <h2>You Win!</h2>
           <button onClick={resetGame}>Play again?</button>
-        </>
+        </div>
+      ) : (
+        <div>
+          <h2>Current Score: {counter} </h2>
+          <button className="button1" onClick={scoreTrack}> +{incrementor}</button>
+          <button className="button1" onClick={upgrade}>
+            Pay 10 points to change from +{incrementor} to +{incrementor + 1}
+          </button>
+        </div>
       )}
     </main>
   );
-      }
+}
 
-      export default App;
+export default App;
