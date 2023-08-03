b0baadb5b58aaa51aa3a75e1a1b1e9da761d8f62
Nicole Slater <nicoleslater@pursuit.org>
2023-08-03 14:00:35 -0400

    UseState

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,43 @@
-import React from "react";
-import "./App.css";
-import { useState } from 'react'
+import React, { useState } from 'react';
 
-function App () {
-  const [counter,setCounter] = useState(0);
-  function handleClick(){
-    setCounter(counter+10);
-    setCounter(counter+2);
-  }
-    return (
-      <main>
-        <p>{counter} namen</p>
+function App() {
+  const [score, setScore] = useState(0);
+  const [increment, setIncrement] = useState(1);
+
+  const handleClick = () => {
+    setScore((prevScore) => prevScore + increment);
+  };
+
+  const handleUpgrade = () => {
+    if (score >= 10) {
+      setScore((prevScore) => prevScore - 10);
+      setIncrement((prevIncrement) => prevIncrement + 1);
+    } else {
+      alert("You can't afford that!");
+    }
+  };
+
+  const handleRestart = () => {
+    setScore(0);
+    setIncrement(1);
+  };
+
+  return (
+    <div className="App">
+      {score >= 100 ? (
         <div>
-        <button onClick={handleClick}>Counter</button>
+          <h2>You Win!</h2>
+          <button onClick={handleRestart}>Play again?</button>
         </div>
-       
-      </main>
-    );
+      ) : (
+        <div>
+          <p>Current Score: {score}</p>
+          <button class="button" onClick={handleClick}>+{increment}</button>
+          <button onClick={handleUpgrade}>
+            Pay 10 points to change from +{increment} to +{increment + 1}
+          </button>
+        </div>
+      )}
+    </div>
+  );
 }
-
-export default App;
