ca8ebcd5d994af0768914a9f7aa030be1f00b72c
Nicole Slater <nicoleslater@pursuit.org>
2023-08-03 14:12:29 -0400

    Adjusting App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,41 @@
 import React, { useState } from 'react';
 
 function App() {
-  const [score, setScore] = useState(0);
-  const [increment, setIncrement] = useState(1);
+  let [counter, setCounter] = useState(0);
+  let [increment, setIncrement] = useState(1);
 
-  const handleClick = () => {
-    setScore((prevScore) => prevScore + increment);
+  const addScore = () => {
+    setCounter((counter += increment));
   };
 
-  const handleUpgrade = () => {
-    if (score >= 10) {
-      setScore((prevScore) => prevScore - 10);
-      setIncrement((prevIncrement) => prevIncrement + 1);
-    } else {
-      alert("You can't afford that!");
-    }
+  const addIncrement = () => {
+    counter > 9
+      ? (setCounter(counter - 10), setIncrement(increment + 1))
+      : alert("You can't afford that!");
   };
 
-  const handleRestart = () => {
-    setScore(0);
-    setIncrement(1);
+  const resetGame = () => {
+    setCounter((counter = 0));
+    setIncrement((increment = 1));
   };
 
   return (
-    <div className="App">
-      {score >= 100 ? (
-        <div>
-          <h2>You Win!</h2>
-          <button onClick={handleRestart}>Play again?</button>
-        </div>
-      ) : (
-        <div>
-          <p>Current Score: {score}</p>
-          <button class="button" onClick={handleClick}>+{increment}</button>
-          <button onClick={handleUpgrade}>
+    <main>
+      <h2>Current Score: {counter}</h2>
+      {counter < 99 ? (
+        <>
+        <button onClick={addScore}>+{increment}</button>
+          <p></p>
+          <button onClick={addIncrement}>
             Pay 10 points to change from +{increment} to +{increment + 1}
           </button>
-        </div>
+        </>
+      ) : (
+        <>
+          <h2>You have won!</h2>
+          <button onClick={resetGame}>Play again?</button>
+        </>
       )}
-    </div>
+    </main>
   );
-}
+      }
